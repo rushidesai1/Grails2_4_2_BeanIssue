7f331c57929ba219aea0dcb3aa16f2b4c35c51c0
Jeff Scott Brown <brownj@ociweb.com>
2016-08-25 07:56:27 -0700

    Simplify the test
    I think this more clearly demonstrates what is going on.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,33 +1,9 @@
-import test.TestA
 import test.TestObject
 
 // Place your Spring DSL code here
 beans = {
-
-    list(ArrayList, ["list1BeanFromResource.groovy"]) { bean ->
-        bean.scope = "prototype"
-    }
-
-    map(HashMap, ["key1BeanFromResource.groovy": "val1BeanFromResource.groovy"]) { bean ->
-        bean.scope = "prototype"
-    }
-
-    testA(TestA) { bean ->
-        bean.scope = "prototype"
-        name = "nameBeanFromResource.groovy"
-    }
-
     testObject(TestObject) { bean ->
         bean.scope = "prototype"
-        a = "aFromResource.groovy"
-        mapIssue1 = ["key1FromResource.groovy": "value1FromResource.groovy"]
-        mapIssue2 = new HashMap(["key2FromResource.groovy": "value2FromResource.groovy"])//["key": "value"]
-        mapIssue3 = new HashMap() {{put("key1FromResource.groovy", "value3FromResource.groovy")}}   //Java way
-        listIssue = ["el1"]
-        //If we use the bean of map we get desired result i.e. Map will not be singleton
-        mapFix = ref("map")
-        listFix = ref("list")
-        testA1 = new TestA(name: "name1FromResource.groovy")
-        testA2 = ref(testA)
+        mapIssue1 = [:]
     }
 }
--- src/groovy/test/test.TestObject.groovy
+++ src/groovy/test/test.TestObject.groovy
@@ -1,16 +1,5 @@
 package test
-/**
- * Created by desair4 on 8/16/2016.
- */
+
 class TestObject {
-    String a
-    String b
     Map mapIssue1
-    Map mapIssue2
-    Map mapIssue3
-    Map mapFix
-    List listIssue
-    List listFix
-    TestA testA1
-    TestA testA2
 }
